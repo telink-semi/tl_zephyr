ffa7d8a0cf56bb64d0bae11e42fcf7a993fa69fe
Serhii Salamakha <serhii.salamakha@gmail.com>
2025-02-11 11:33:04 +0200

    telink: net: kconfig: use native zephyr ethernet L2 for w91
    Used native zephyr ethernet L2 for w91
    
    Signed-off-by: Serhii Salamakha <serhii.salamakha@gmail.com>

---
+++ drivers/ethernet/eth_w91.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Telink Semiconductor (Shanghai) Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* This file is intentionally left empty to suppress a CMake warning:
+ *
+ * CMake Warning at /zephyr/CMakeLists.txt:844 (message):
+ *   No SOURCES given to Zephyr library: drivers__ethernet
+ *   Excluding target from build.
+ */
