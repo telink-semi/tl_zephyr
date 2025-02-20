b0f6ab99f6c9dc5b597aa547b767c774449df3d6
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2025-02-20 17:07:47 +0200

    Provide command response to spinel.

--- soc/telink/tlsr/telink_w9x/ot_rcp/ot_rcp.c
+++ soc/telink/tlsr/telink_w9x/ot_rcp/ot_rcp.c
@@ -5,6 +5,8 @@
  */
 
 #include "ot_rcp.h"
+#include <stdlib.h>
+
 #include "spinel_drv.h"
 
 #define LOG_LEVEL CONFIG_IEEE802154_DRIVER_LOG_LEVEL
@@ -93,8 +95,21 @@ static void openthread_rcp_reception(uint8_t bt, const void *ctx)
 {
 	struct openthread_rcp_data *ot_rcp = (struct openthread_rcp_data *)ctx;
 
-	LOG_INF("spinel rx [%p] %02x", ot_rcp, bt);
-	/* TODO: unpack using spinel */
+	if (!ot_rcp->spinel_rx_buffer.data) {
+		ot_rcp->spinel_rx_buffer.data = malloc(CONFIG_TELINK_W91_OT_SPINEL_RX_BUFFER_SIZE);
+		ot_rcp->spinel_rx_buffer.data_size = 0;
+	}
+
+	if (ot_rcp->spinel_rx_buffer.data) {
+		if (ot_rcp->spinel_rx_buffer.data_size < CONFIG_TELINK_W91_OT_SPINEL_RX_BUFFER_SIZE) {
+			ot_rcp->spinel_rx_buffer.data[ot_rcp->spinel_rx_buffer.data_size] = bt;
+			ot_rcp->spinel_rx_buffer.data_size ++;
+		} else {
+			LOG_ERR("spinel rx buffer overflows");
+		}
+	} else {
+		LOG_ERR("spinel can't allocate rx buffer");
+	}
 }
 
 static void openthread_rcp_reception_done(bool data_valid, const void *ctx)
@@ -102,8 +117,12 @@ static void openthread_rcp_reception_done(bool data_valid, const void *ctx)
 	if (data_valid) {
 		struct openthread_rcp_data *ot_rcp = (struct openthread_rcp_data *)ctx;
 
-		LOG_INF("spinel rx finish [%p]", ot_rcp);
-		/* TODO: analyze received spinel data */
+		if (ot_rcp->spinel_rx_buffer.data) {
+			ot_rcp->spinel_rx_buffer.data_size -= HDLC_CODER_LENGTH_CRC;
+			/* TODO: check if response or stream data */
+			k_msgq_put(&ot_rcp->spinel_msgq, &ot_rcp->spinel_rx_buffer, K_FOREVER);
+			ot_rcp->spinel_rx_buffer.data = NULL;
+		}
 	}
 }
 
@@ -129,6 +148,9 @@ int openthread_rcp_init(struct openthread_rcp_data *ot_rcp, const struct device 
 		hdlc_coder_out_data_set(&ot_rcp->hdlc, openthread_rcp_hdlc_transmission);
 		hdlc_coder_inp_data_set(&ot_rcp->hdlc, openthread_rcp_reception);
 		hdlc_coder_inp_finish_set(&ot_rcp->hdlc, openthread_rcp_reception_done);
+		ot_rcp->spinel_rx_buffer.data = NULL;
+		k_msgq_init(&ot_rcp->spinel_msgq, (char *)&ot_rcp->spinel_msgq_buffer,
+			sizeof(struct openthread_rcp_rx_buffer), ARRAY_SIZE(ot_rcp->spinel_msgq_buffer));
 
 		if (uart_irq_callback_user_data_set(ot_rcp->uart,
 			openthread_rcp_reception_isr, ot_rcp)) {
@@ -158,6 +180,7 @@ int openthread_rcp_deinit(struct openthread_rcp_data *ot_rcp)
 
 		(void) k_work_cancel_sync(&ot_rcp->work, &work_sync);
 		ring_buf_reset(&ot_rcp->rb);
+		k_msgq_purge(&ot_rcp->spinel_msgq);
 	} while (0);
 
 	return result;
@@ -167,63 +190,35 @@ int openthread_rcp_reset(struct openthread_rcp_data *ot_rcp)
 {
 	LOG_INF("%s", __func__);
 
-	int ret = spinel_drv_send_reset(0, openthread_rcp_spinel_transmission, ot_rcp,
+	int result = spinel_drv_send_reset(0, openthread_rcp_spinel_transmission, ot_rcp,
 		SPINEL_RESET_STACK);
 
-	if (ret < 0) {
-		LOG_ERR("Failed to send spinel reset command (err = %d)", ret);
-		return ret;
+	hdlc_coder_out_finish(&ot_rcp->hdlc, result >= 0);
+	if (result >= 0) {
+		result = -ETIMEDOUT;
+		k_timepoint_t t = sys_timepoint_calc(
+			K_MSEC(CONFIG_TELINK_W91_OT_SPINEL_RESPONSE_TIMEOUT_MS));
+
+		while (!sys_timepoint_expired(t)) {
+			struct openthread_rcp_rx_buffer response;
+
+			if (!k_msgq_get(&ot_rcp->spinel_msgq, &response, sys_timepoint_timeout(t))) {
+				LOG_HEXDUMP_INF(response.data, response.data_size, "rx");
+				if (spinel_drv_check_reset(ot_rcp, response.data, response.data_size)) {
+					free(response.data);
+					result = 0;
+					break;
+				} else {
+					LOG_WRN("spinel trash received");
+					free(response.data);
+				}
+			}
+		}
+		if (result == -ETIMEDOUT) {
+			LOG_ERR("spinel response timeout");
+		}
+	} else {
+		LOG_ERR("spinel encoding error");
 	}
-
-	hdlc_coder_out_finish(&ot_rcp->hdlc, true);
-
-#if 0
-	int8_t t_id = 0;
-
-	// Test semd get protocol version command
-	ret = spinel_drv_send_cmd(0, t_id, openthread_rcp_spinel_transmission, ot_rcp,
-		SPINEL_CMD_PROP_VALUE_GET, SPINEL_PROP_PROTOCOL_VERSION, NULL);
-
-	if (ret < 0) {
-		LOG_ERR("Failed to send spinel command (err = %d)", ret);
-		return ret;
-	}
-
-	hdlc_coder_out_finish(&ot_rcp->hdlc, true);
-
-	// Test stream raw command
-	bool test_bool = true;
-	uint8_t test_uint8 = 0x10;
-	uint16_t test_uint16 = 0x3322;
-	uint32_t test_uint32 = 0x77665544;
-	uint64_t test_uint64 = 0xFFEEDDCCBBAA9988;
-	unsigned int test_uint = 128; // 80 01
-	const char *test_utf8 = "Hello"; // 48 65 6C 6C 6F 00
-
-	uint8_t test_data_wlen[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	ret = spinel_drv_send_cmd(0, t_id, openthread_rcp_spinel_transmission, ot_rcp,
-		SPINEL_CMD_PROP_VALUE_SET, SPINEL_PROP_STREAM_RAW,
-		SPINEL_DATATYPE_BOOL_S
-		SPINEL_DATATYPE_UINT8_S
-		SPINEL_DATATYPE_UINT16_S
-		SPINEL_DATATYPE_UINT32_S
-		SPINEL_DATATYPE_UINT64_S
-		SPINEL_DATATYPE_UINT_PACKED_S
-		SPINEL_DATATYPE_UTF8_S
-		SPINEL_DATATYPE_DATA_WLEN_S,
-		test_bool, test_uint8, test_uint16, test_uint32, test_uint64,
-		test_uint, test_utf8, test_data_wlen, sizeof(test_data_wlen));
-
-	if (ret < 0) {
-		LOG_ERR("Failed to send spinel data (err = %d)", ret);
-		return ret;
-	}
-
-	hdlc_coder_out_finish(&ot_rcp->hdlc, true);
-
-#endif
-
-	/* TODO: wait for response */
-
-	return 0;
+	return result;
 }
--- soc/telink/tlsr/telink_w9x/ot_rcp/spinel_drv.h
+++ soc/telink/tlsr/telink_w9x/ot_rcp/spinel_drv.h
@@ -12,6 +12,7 @@
 #include "spinel.h"
 
 int spinel_drv_send_reset(uint8_t inst, spinel_tx_cb tx_cb, const void *ctx, uint8_t type);
+bool spinel_drv_check_reset(const void *ctx, uint8_t *data, size_t data_size);
 int spinel_drv_send_cmd(uint8_t inst, int8_t t_id, spinel_tx_cb tx_cb, const void *ctx,
 	uint32_t cmd, uint32_t prop, const char *fmt, ...);
 
