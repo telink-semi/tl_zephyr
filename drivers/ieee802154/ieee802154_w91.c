08599100f7dcffb0e267e44e15b012af3fcb53a1
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2025-03-12 17:20:25 +0200

    Update W91 radio driver.

--- drivers/ieee802154/ieee802154_w91.c
+++ drivers/ieee802154/ieee802154_w91.c
@@ -35,6 +35,18 @@ static int w91_zb_init(const struct device *dev)
 static const struct ieee802154_radio_api w91_zb_drv_api = {
 };
 
+#if CONFIG_NET_L2_IEEE802154 || CONFIG_NET_L2_OPENTHREAD
+
+#if CONFIG_NET_L2_IEEE802154
+#define W91_ZB_L2 IEEE802154_L2
+#define W91_ZB_L2_CTX_TYPE NET_L2_GET_CTX_TYPE(IEEE802154_L2)
+#define W91_ZB_MTU 125
+#elif CONFIG_NET_L2_OPENTHREAD
+#define W91_ZB_L2 OPENTHREAD_L2
+#define W91_ZB_L2_CTX_TYPE NET_L2_GET_CTX_TYPE(OPENTHREAD_L2)
+#define W91_ZB_MTU 1280
+#endif /* net L2 select */
+
 #define W91_ZB_DEFINE(n)                                        \
                                                                 \
 	static const struct w91_zb_config w91_zb_config_##n = {     \
@@ -45,8 +57,24 @@ static const struct ieee802154_radio_api w91_zb_drv_api = {
 	NET_DEVICE_DT_INST_DEFINE(n, w91_zb_init,                   \
 		NULL, &w91_zb_data_##n, &w91_zb_config_##n,             \
 		UTIL_INC(CONFIG_TELINK_W91_IPC_DRIVERS_INIT_PRIORITY),  \
-		&w91_zb_drv_api, OPENTHREAD_L2,                         \
-		NET_L2_GET_CTX_TYPE(OPENTHREAD_L2), 1280);
+		&w91_zb_drv_api, W91_ZB_L2,                             \
+		W91_ZB_L2_CTX_TYPE, W91_ZB_MTU);
 
+#else
+
+#define W91_ZB_DEFINE(n)                                        \
+                                                                \
+	static const struct w91_zb_config w91_zb_config_##n = {     \
+	};                                                          \
+                                                                \
+	static struct w91_zb_data w91_zb_data_##n;                  \
+                                                                \
+	DEVICE_DT_INST_DEFINE(n, w91_zb_init,                       \
+		NULL, &w91_zb_data_##n, &w91_zb_config_##n,             \
+		POST_KERNEL,                                            \
+		UTIL_INC(CONFIG_TELINK_W91_IPC_DRIVERS_INIT_PRIORITY),  \
+		&w91_zb_drv_api);
+
+#endif /* CONFIG_NET_L2_IEEE802154 || CONFIG_NET_L2_OPENTHREAD */
 
 DT_INST_FOREACH_STATUS_OKAY(W91_ZB_DEFINE)
