66c6ca4c968b1828af034dc2a8c2932627f566c2
Serhii Salamakha <serhii.salamakha@gmail.com>
2025-03-12 17:21:28 +0200

    telink: cmake: Kconfig: add spinel and spinel_drv modules for W91
    Added spinel and spinel_drv modules for W91
    
    Signed-off-by: Serhii Salamakha <serhii.salamakha@gmail.com>

---
+++ soc/telink/tlsr/telink_w9x/ot_rcp/spinel_drv.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 Telink Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SPINEL_DRV_H
+#define SPINEL_DRV_H
+
+#include <zephyr/kernel.h>
+
+#include "spinel.h"
+
+int spinel_drv_send_reset(uint8_t inst, spinel_tx_cb tx_cb, const void *ctx, uint8_t type);
+int spinel_drv_send_cmd(uint8_t inst, int8_t t_id, spinel_tx_cb tx_cb, const void *ctx,
+	uint32_t cmd, uint32_t prop, const char *fmt, ...);
+
+#endif /* SPINEL_DRV_H */
