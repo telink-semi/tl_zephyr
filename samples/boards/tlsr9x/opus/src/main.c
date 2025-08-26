c0ab5c41ef52e9cf677692f01e7289d44a12150c
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2025-08-26 17:13:00 +0300

    soc: telink: common: opus: Add OPUS codec
    Added OPUS codec SW unit. This unit provides encoding/decoding
    functionality to N22 core via IPC.
    For usage refer to: samples/boards/tlsr9x/opus
    
    Signed-off-by: Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>

---
+++ samples/boards/tlsr9x/opus/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Telink Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+int main(void)
+{
+	LOG_INF("***** Telink OPUS demo *****");
+
+	return 0;
+}
