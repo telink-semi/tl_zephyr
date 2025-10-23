0b3379032b52a962d6be31f8c98372ebe2b8c2de
Haiwen Xia <haiwen.xia@telink-semi.com>
2025-10-23 13:59:33 +0800

    telink: tl3238x: adopt drivers change.
    - adopt drivers change outside.
    
    Signed-off-by: Haiwen Xia <haiwen.xia@telink-semi.com>

--- soc/telink/tlsr/telink_tlx/soc.c
+++ soc/telink/tlsr/telink_tlx/soc.c
@@ -28,6 +28,16 @@
 # endif
 #endif
 
+/* Drivers changes , so should not change castart.s, add external*/
+#if CONFIG_SOC_RISCV_TELINK_TL323X
+_attribute_data_retention_sec_ unsigned int g_pm_mspi_cfg;
+__attribute__((section(".ram_code_retention"))) __attribute__((noinline)) 
+void pm_retention_register_recover(void)
+{
+   
+}
+#endif
+
 /* Power Mode value */
 #if CONFIG_SOC_RISCV_TELINK_TL321X
 	/* List of supported CCLK frequencies */
