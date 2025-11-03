c0de665355c18a1eef0397d8433b1c7de70b15ef
Zhihan Tao <zhihan.tao@telink-semi.com>
2025-11-03 17:07:19 +0800

    feat(tl321x): sync with src.
    repo: tl_ble_src/develop
    
    SHA-1: 518bb6d96dc248fc58e1a600206525b9c160356e
    
    * Merge branch 'Develop' of http://192.168.48.36/src/ble/telink_b91m_ble_multi_connection_src into Develop
    
    Signed-off-by: Zhihan Tao <zhihan.tao@telink-semi.com>

--- soc/telink/tlsr/telink_tlx/soc.c
+++ soc/telink/tlsr/telink_tlx/soc.c
@@ -29,12 +29,10 @@
 #endif
 
 /* Drivers changes , so should not change castart.s, add external*/
-#if CONFIG_SOC_RISCV_TELINK_TL323X
+#if CONFIG_SOC_RISCV_TELINK_TL321X || CONFIG_SOC_RISCV_TELINK_TL323X
 _attribute_data_retention_sec_ unsigned int g_pm_mspi_cfg;
-__attribute__((section(".ram_code_retention"))) __attribute__((noinline)) 
-void pm_retention_register_recover(void)
-{
-   
+__attribute__((section(".ram_code_retention"))) __attribute__((noinline))
+void pm_retention_register_recover(void){
 }
 #endif
 
