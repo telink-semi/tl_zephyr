4786b864f2d0ed0d814c9f2794d59f465307ccf9
chong.song <chong.song@telink-semi.com>
2025-11-04 20:06:24 +0800

    feat(zephyr): add peripheral_kmd prj files

---
+++ samples/bluetooth/peripheral_kmd/src/app_usb.h
@@ -0,0 +1,59 @@
+/** @file
+ *  @brief HoG Service sample
+ */
+
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define HID_USAGE_GEN_DESKTOP_CONSUMER        0x0C
+
+/**
+ * @brief Simple HID N-keys keyboard report descriptor.
+ */
+#define HID_N_KEY_REPORT_DESC() {                                \
+        HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),                \
+        HID_USAGE(HID_USAGE_GEN_DESKTOP_KEYBOARD),        \
+        HID_COLLECTION(HID_COLLECTION_APPLICATION),        \
+                HID_REPORT_ID(0x08),                                                \
+                HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP_KEYPAD),\
+                HID_USAGE_MIN8(0x00),                                \
+                HID_USAGE_MAX8(0x80),                                \
+                HID_LOGICAL_MIN8(0),                                \
+                HID_LOGICAL_MAX8(1),                                \
+                HID_REPORT_SIZE(1),                                        \
+                HID_REPORT_COUNT(0x80),                                \
+                HID_INPUT(0x02),                                        \
+        HID_END_COLLECTION,                                                \
+                                                                                        \
+                                /*Consumer */                                \
+        HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP_CONSUMER),                \
+        HID_USAGE(HID_USAGE_GEN_DESKTOP),        \
+        HID_COLLECTION(HID_COLLECTION_APPLICATION),        \
+                HID_REPORT_ID(0x02),                                        \
+                HID_REPORT_SIZE(0x10),                                        \
+                HID_REPORT_COUNT(0x01),                                \
+                HID_LOGICAL_MIN8(1),                                \
+                HID_LOGICAL_MAX16(0x8c, 0x02),                                \
+                HID_USAGE_MIN8(0x00),                                \
+                HID_USAGE_MAX16(0x8c, 0x02),                                \
+                HID_INPUT(0x00),                                        \
+        HID_END_COLLECTION,                                                \
+}
+
+extern volatile unsigned int vbus_status;
+extern volatile  unsigned int usb_connected_ok;
+
+int usb_hw_init(void);
+void usb_test_loop(void);
+void app_usb_main_loop(void);
+_attribute_ram_code_sec_ void app_usb_status_check(void);
+#ifdef __cplusplus
+}
+#endif
---
+++ samples/bluetooth/peripheral_kmd/src/main.c
@@ -0,0 +1,51 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/settings/settings.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+#include "app_public.h"
+
+#include "keyscan_ana.h"
+
+#define LOG_LEVEL LOG_LEVEL_DBG
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+
+int main(void)
+{
+
+	keyboard_comm_init();
+
+	#if USB_APP_FUN_ENABLE
+	/*usb init*/
+	usb_hw_init();
+	#endif
+
+	while(1) {
+
+	    public_loop();
+		k_sleep(K_MSEC(3));
+	}
+
+	return 0;
+}
