84ec60b9f09288c2857de1977531215c428a343b
Hao <hao.wu@telink-semi.com>
2026-01-08 10:58:14 +0800

    telink: tl323x: reduce trim dig ldo and disable peripheral module clocks. - reduce trim dig ldo when enable PM. - disable some default-enabled peripheral module clocks when enable PM.
    Signed-off-by: Hao <hao.wu@telink-semi.com>

--- soc/telink/tlsr/telink_tlx/soc.c
+++ soc/telink/tlsr/telink_tlx/soc.c
@@ -194,6 +194,71 @@ void soc_load_rf_parameters_deep_retention(void)
 }
 #endif
 
+#if CONFIG_PM
+#define RST_BIT_CLR(x, n)    ((x) |= (n))
+#define CLOCK_BIT_CLR(x, n)    ((x) &=~(n))
+__attribute__((noinline)) __attribute__((section(".ram_code"))) __attribute__((optimize("O2")))
+void gen_fsk_close_unused_clock(void)
+{
+	RST_BIT_CLR(reg_rst0, FLD_RST0_I2C0);
+    RST_BIT_CLR(reg_rst0, FLD_RST0_UART1);
+    
+    RST_BIT_CLR(reg_rst1, FLD_RST1_UART3);
+    RST_BIT_CLR(reg_rst1, FLD_RST1_GSPI);
+    RST_BIT_CLR(reg_rst1, FLD_RST1_DMA);
+    RST_BIT_CLR(reg_rst1, FLD_RST1_SPISLV);
+    
+    RST_BIT_CLR(reg_rst2, FLD_RST2_I2C1);
+	RST_BIT_CLR(reg_rst2, FLD_RST2_LM);
+	RST_BIT_CLR(reg_rst2, FLD_RST2_TRNG);
+    
+    RST_BIT_CLR(reg_rst3, FLD_RST3_QDEC1);
+	RST_BIT_CLR(reg_rst3, FLD_RST3_TRACE);
+	RST_BIT_CLR(reg_rst3, FLD_RST3_BROM);
+    RST_BIT_CLR(reg_rst3, FLD_RST3_QDEC);
+    
+    RST_BIT_CLR(reg_rst4, FLD_RST4_DC);
+    RST_BIT_CLR(reg_rst4, FLD_RST4_UART4);
+    RST_BIT_CLR(reg_rst4, FLD_RST4_SKE);
+    RST_BIT_CLR(reg_rst4, FLD_RST4_HASH);	// will enable when HW HASH used
+    
+    RST_BIT_CLR(reg_rst5, FLD_RST5_UART2);
+    RST_BIT_CLR(reg_rst5, FLD_RST5_PEM);
+    
+    RST_BIT_CLR(reg_rst6, FLD_RST6_RZ);
+    
+    RST_BIT_CLR(reg_rst7, FLD_RST7_USB1);
+
+	CLOCK_BIT_CLR(reg_clk_en0, FLD_CLK0_LSPI_EN);
+	CLOCK_BIT_CLR(reg_clk_en0, FLD_CLK0_I2C0_EN);
+	CLOCK_BIT_CLR(reg_clk_en0, FLD_CLK0_UART1_EN);
+
+	CLOCK_BIT_CLR(reg_clk_en1, FLD_CLK0_UART3_EN);
+	CLOCK_BIT_CLR(reg_clk_en1, FLD_CLK1_DMA_EN);
+	CLOCK_BIT_CLR(reg_clk_en1, FLD_CLK1_GSPI_EN);
+	CLOCK_BIT_CLR(reg_clk_en1, FLD_CLK1_SPISLV_EN);
+
+	CLOCK_BIT_CLR(reg_clk_en2, FLD_CLK2_I2C1_EN);
+
+	CLOCK_BIT_CLR(reg_clk_en3, FLD_CLK3_QDEC1_EN);
+	CLOCK_BIT_CLR(reg_clk_en3, FLD_CLK3_TRACE_EN);
+	CLOCK_BIT_CLR(reg_clk_en3, FLD_CLK3_BROM_EN);
+	CLOCK_BIT_CLR(reg_clk_en3, FLD_CLK3_QDEC0_EN);
+
+	CLOCK_BIT_CLR(reg_clk_en4, FLD_CLK4_DC_EN);
+	CLOCK_BIT_CLR(reg_clk_en4, FLD_CLK4_UART4_EN);
+	CLOCK_BIT_CLR(reg_clk_en4, FLD_CLK4_SKE_EN);
+	CLOCK_BIT_CLR(reg_clk_en4, FLD_CLK4_HASH_EN);	// will enable when HW HASH used
+
+	CLOCK_BIT_CLR(reg_clk_en5, FLD_CLK5_UART2_EN);
+	CLOCK_BIT_CLR(reg_clk_en5, FLD_CLK5_PEM_EN);
+
+	CLOCK_BIT_CLR(reg_clk_en6, FLD_CLK6_RZ_EN);
+
+	CLOCK_BIT_CLR(reg_clk_en7, FLD_CLK7_USB1_EN);
+}
+#endif /* CONFIG_PM  */
+
 /**
  * @brief Perform basic initialization at boot.
  *
@@ -245,6 +310,9 @@ void soc_early_init_hook(void)
 		PLL_192M_D25F_48M_HCLK_N22_24M_PCLK_12M_MSPI_48M;
 #elif CONFIG_SOC_RISCV_TELINK_TL323X
 		PLL_192M_CCLK_48M_HCLK_24M_PCLK_12M_MSPI_48M;
+		#if CONFIG_PM
+			pm_set_dig_ldo_voltage(DIG_LDO_TRIM_0P850V);
+		#endif /* CONFIG_PM  */
 #elif CONFIG_SOC_RISCV_TELINK_TL721X
 		PLL_240M_CCLK_48M_HCLK_48M_PCLK_48M_MSPI_48M;
 #endif /* CONFIG_SOC_RISCV_TELINK_TLX */
@@ -383,6 +451,10 @@ void soc_tlx_restore(void)
 		PLL_192M_D25F_48M_HCLK_N22_24M_PCLK_12M_MSPI_48M;
 #elif CONFIG_SOC_RISCV_TELINK_TL323X
 		PLL_192M_CCLK_48M_HCLK_24M_PCLK_12M_MSPI_48M;
+		#if CONFIG_PM
+			pm_set_dig_ldo_voltage(DIG_LDO_TRIM_0P850V);
+			gen_fsk_close_unused_clock();
+		#endif /* CONFIG_PM  */
 #elif CONFIG_SOC_RISCV_TELINK_TL721X
 		PLL_240M_CCLK_48M_HCLK_48M_PCLK_48M_MSPI_48M;
 #endif /* CONFIG_SOC_RISCV_TELINK_TLX */
