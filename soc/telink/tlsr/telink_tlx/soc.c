6e5ca54d7327392ff7d1f6c877b09cf623b0b630
haiwen.xia <haiwen.xia@telink-semi.com>
2026-01-20 20:22:54 +0800

    telink: tl323x: update core voltage to 0.9v.
    - 0.9v in lowpower mode will be more stable.
    
    Signed-off-by: haiwen.xia <haiwen.xia@telink-semi.com>

--- soc/telink/tlsr/telink_tlx/soc.c
+++ soc/telink/tlsr/telink_tlx/soc.c
@@ -321,7 +321,7 @@ void soc_early_init_hook(void)
 #elif CONFIG_SOC_RISCV_TELINK_TL323X
 		PLL_192M_CCLK_48M_HCLK_24M_PCLK_12M_MSPI_48M;
 		#if CONFIG_PM
-			pm_set_dig_ldo_voltage(DIG_LDO_TRIM_0P850V);
+			pm_set_dig_ldo_voltage(DIG_LDO_TRIM_0P900V);
 		#endif /* CONFIG_PM  */
 #elif CONFIG_SOC_RISCV_TELINK_TL721X
 		PLL_240M_CCLK_48M_HCLK_48M_PCLK_48M_MSPI_48M;
