508025c7003d227c2732b3d61b8503a15681dce0
haiwen.xia <haiwen.xia@telink-semi.com>
2026-01-20 20:24:38 +0800

    telink: tl323x: update uart driver for pm mode.
    - fix bugs for only pm action resume need uart driver init.
    
    Signed-off-by: haiwen.xia <haiwen.xia@telink-semi.com>

--- drivers/serial/uart_tlx.c
+++ drivers/serial/uart_tlx.c
@@ -642,7 +642,6 @@ static int uart_tlx_pm_action(const struct device *dev, enum pm_device_action ac
 			}
 		}
 #endif /* CONFIG_SOC_SERIES_RISCV_TELINK_TLX_RETENTION */
-		uart_tlx_driver_init(dev);
 		/* reset TX/RX byte index */
 		data->tx_byte_index = 0;
 		data->rx_byte_index = 0;
@@ -652,7 +651,6 @@ static int uart_tlx_pm_action(const struct device *dev, enum pm_device_action ac
 		break;
 
 	case PM_DEVICE_ACTION_SUSPEND:
-		uart_tlx_driver_init(dev);
 		// /* reset TX/RX byte index */
 		data->tx_byte_index = 0;
 		data->rx_byte_index = 0;
