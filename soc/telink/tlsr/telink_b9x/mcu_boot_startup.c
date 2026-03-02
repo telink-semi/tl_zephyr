620e6fed966ea56691a5d0e0d9ed3421fc9dc46d
Hao Wu <hao.wu@telink-semi.com>
2026-03-02 16:04:52 +0800

    telink: b92: output the chip ID when RX is grounded
    - Output the chip ID when RX is grounded.
    
    Signed-off-by: Hao Wu <hao.wu@telink-semi.com>

--- soc/telink/tlsr/telink_b9x/mcu_boot_startup.c
+++ soc/telink/tlsr/telink_b9x/mcu_boot_startup.c
@@ -8,10 +8,17 @@
 #include <zephyr/drivers/uart.h>
 #include <bootutil/bootutil_log.h>
 #include <zephyr/sys/crc.h>
+
+#if CONFIG_SOC_RISCV_TELINK_B92
+#include "gpio.h"
+#include <zephyr/dt-bindings/pinctrl/b92-pinctrl.h>
+#endif
+
 BOOT_LOG_MODULE_REGISTER(telink_b9x_mcuboot);
 
 static bool telink_b9x_mcu_boot_startup(void);
-static __maybe_unused void printk_buf(const char *comment, void *buf, size_t buf_len);
+
+void print_buffer(uint8_t *buffer, size_t size);
 
 void __wrap_main(void)
 {
@@ -27,7 +34,6 @@ static bool telink_b9x_mcu_boot_startup(void)
 {
 	bool result = true; /* run MCUBoot main */
 
-	BOOT_LOG_INF("telink B9x MCUBoot on early boot");
 #if CONFIG_SOC_RISCV_TELINK_B92
 	bool show_chip_id = false;
 
@@ -35,15 +41,24 @@ static bool telink_b9x_mcu_boot_startup(void)
 	const struct device *const uart_con = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 	if (device_is_ready(uart_con)) {
-		/* Get UART pins count shorted to ground */
-		int pins_gnd = uart_drv_cmd(uart_con, 0, 0);
+	/* Use the uart corresponding to your zephyr_console configuration */
+		#define UART_RX_PINMUX \
+			DT_PROP(DT_PINCTRL_BY_IDX(DT_NODELABEL(uart0), 0, 1), pinmux)
+		gpio_pin_e uart_rx = B9x_PINMUX_GET_PIN(UART_RX_PINMUX);
 
-		if (pins_gnd >= 0) {
-			if (pins_gnd > 0) {
-				show_chip_id = true;
-			}
-		} else {
-			BOOT_LOG_ERR("can't count uart shorted to GND pins");
+		/* Disable The UART RX PIN */
+		gpio_set_low_level(uart_rx);
+		gpio_output_dis(uart_rx);
+		gpio_function_dis(uart_rx);
+
+		/* Enable The UART RX PIN GPIO Function */
+		gpio_function_en(uart_rx);
+		gpio_input_en(uart_rx);
+		gpio_set_up_down_res(uart_rx, GPIO_PIN_PULLUP_10K);
+
+		/* Check if Console UART RX PIN is at low level - shorted to ground */
+		if (gpio_get_level(uart_rx) == 0) {
+			show_chip_id = true;
 		}
 	} else {
 		BOOT_LOG_ERR("uart console not ready");
@@ -51,14 +66,17 @@ static bool telink_b9x_mcu_boot_startup(void)
 
 	if (show_chip_id) {
 		extern unsigned char efuse_get_chip_id(unsigned char *chip_id_buff);
-		uint8_t chip_id[18];
+		uint8_t chip_id[21] = {0};
 
-		if (efuse_get_chip_id(chip_id)) {
-			uint16_t chip_id_crc = crc16_itu_t(0, chip_id, 16);
+		if (efuse_get_chip_id(chip_id + 2)) {
+			uint16_t chip_id_crc = crc16_itu_t(0, chip_id + 2, 16);
 
-			chip_id[16] = chip_id_crc & 0x00ff;
-			chip_id[17] = chip_id_crc >> 8;
-			printk_buf("chip id", chip_id, sizeof(chip_id));
+			chip_id[0] = 0xaa;
+			chip_id[1] = 0x12;
+			chip_id[18] = chip_id_crc & 0x00ff;
+			chip_id[19] = chip_id_crc >> 8;
+			chip_id[20] = 0x55;
+			print_buffer(chip_id, sizeof(chip_id));
 		} else {
 			BOOT_LOG_ERR("chip id read error");
 		}
@@ -67,11 +85,9 @@ static bool telink_b9x_mcu_boot_startup(void)
 	return result;
 }
 
-static __maybe_unused void printk_buf(const char *comment, void *buf, size_t buf_len)
+void print_buffer(uint8_t *buffer, size_t size)
 {
-	printk("%s[%u]:", comment, buf_len);
-	for (size_t i = 0; i < buf_len; ++i) {
-		printk(" %02x", ((uint8_t *)buf)[i]);
+	for (size_t i = 0; i < size; i++) {
+		printk("%c", buffer[i]);
 	}
-	printk("\n");
 }
