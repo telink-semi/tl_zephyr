59b2e7ff9890a280ef00cdd650615165927df43f
fenton1609 <fengtai.xie@telink-semi.com>
2026-04-14 17:51:58 +0800

    Update adc_tl323x.c

--- drivers/adc/adc_tl323x.c
+++ drivers/adc/adc_tl323x.c
@@ -35,7 +35,7 @@ struct tl323x_adc_data {
 	struct k_thread thread;
     uint8_t mode;
 
-	/* To save resources, the ADC thread will not be created because the user is not using it. */
+	/* To save resources, the ADC thread will not be created because the user is not using */
 	/* K_KERNEL_STACK_MEMBER(stack, CONFIG_ADC_TL323X_ACQUISITION_THREAD_STACK_SIZE); */
 };
 
@@ -433,13 +433,14 @@ static int adc_tl323x_init(const struct device *dev)
     }
 
 	k_sem_init(&data->acq_sem, 0, 1);
-	/* To save resources, the ADC thread will not be created because the user is not using it. */
+	/* To save resources, the ADC thread will not be created because the user is not using */
 	/* k_thread_create(&data->thread, data->stack,
 		CONFIG_ADC_TL323X_ACQUISITION_THREAD_STACK_SIZE,
 		(k_thread_entry_t)adc_tl323x_acquisition_thread,
 		(void *)dev, NULL, NULL,
 		CONFIG_ADC_TL323X_ACQUISITION_THREAD_PRIO,
-		0, K_NO_WAIT); */
+		0, K_NO_WAIT);
+	*/
 	adc_context_unlock_unconditionally(&data->ctx);
 
 	return 0;
