25c4824e2f1a3a66fc24bdc91c34818657c7ae67
Zhihan Tao <zhihan.tao@telink-semi.com>
2026-04-25 13:57:35 +0800

    feat: add tl5238x basic porting.
    (cherry picked from commit e920d8f3ad1092cef318eb3b42e83eaf72b0a711)

--- soc/telink/tlsr/telink_tl5x/idle.c
+++ soc/telink/tlsr/telink_tl5x/idle.c
@@ -7,11 +7,17 @@
 #include <zephyr/toolchain.h>
 #include <zephyr/tracing/tracing.h>
 
+#include <plmt.h>
+
 #if CONFIG_ARCH_HAS_CUSTOM_CPU_IDLE || CONFIG_ARCH_HAS_CUSTOM_CPU_ATOMIC_IDLE
 
 /* Due to silicon bug in B92 platform, the WFI emulation is implemented */
 static volatile bool __irq_pending;
+__attribute__((used)) volatile uint32_t mstatus_flag = 0;
+__attribute__((used)) volatile uint32_t mie_flag = 0;
 
+__attribute__((used)) volatile uint64_t AAA_mcmp = 0;
+__attribute__((used)) volatile uint64_t AAA_mtime = 0;
 static ALWAYS_INLINE void riscv_idle(unsigned int key)
 {
 	sys_trace_idle();
@@ -19,20 +25,33 @@ static ALWAYS_INLINE void riscv_idle(unsigned int key)
 	irq_unlock(key);
 
 	/* Wait for interrupt */
-	#if CONFIG_SOC_RISCV_TELINK_TL322X && CONFIG_SOC_RISCV_TELINK_TL323X
+	#if CONFIG_SOC_RISCV_TELINK_TL322X || CONFIG_SOC_RISCV_TELINK_TL523X
+		mstatus_flag = csr_read(mstatus);
+		mie_flag = csr_read(mie);
+		AAA_mcmp = mtime_get_cmp_value();
+		AAA_mtime = mtime_get_value();
 		__asm__ volatile("wfi");
 	#else
+		mstatus_flag = csr_read(mstatus);
+		mie_flag = csr_read(mie);
 		while (__irq_pending) {
+			AAA_mcmp = mtime_get_cmp_value();
+			AAA_mtime = mtime_get_value();
 		}
 	#endif
 }
 
 void __soc_handle_irq(unsigned long mcause)
 {
-	__irq_pending = false;
-	__asm__("li t1, 1");
-	__asm__("sll t0, t1, a0");
-	__asm__("csrrc t1, mip, t0");
+    __irq_pending = false;
+    __asm__ volatile (
+        "li t1, 1\n\t"
+        "sll t0, t1, %0\n\t"
+        "csrrc t1, mip, t0"
+        : 
+        : "r"(mcause)
+        : "t0", "t1", "memory"
+    );
 }
 #endif /* CONFIG_ARCH_HAS_CUSTOM_CPU_IDLE || CONFIG_ARCH_HAS_CUSTOM_CPU_ATOMIC_IDLE */
 
