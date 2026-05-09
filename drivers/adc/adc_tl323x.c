a8b7ccce26a553b3f8b0c0b0ab81e6c88622ed8a
haiwen.xia <haiwen.xia@telink-semi.com>
2026-05-09 10:30:34 +0800

    telink: tl323x: update matter to v1.0.2.0.
    - add 32k wd logic.
    - open hw acceleration for SHA256.
    - adjust core volatage.
    - adjust pmos time for 3.3uH.
    
    Signed-off-by: haiwen.xia <haiwen.xia@telink-semi.com>

--- drivers/adc/adc_tl323x.c
+++ drivers/adc/adc_tl323x.c
@@ -35,7 +35,8 @@ struct tl323x_adc_data {
 	struct k_thread thread;
     uint8_t mode;
 
-	K_KERNEL_STACK_MEMBER(stack, CONFIG_ADC_TL323X_ACQUISITION_THREAD_STACK_SIZE);
+	/* To save resources, the ADC thread will not be created because the user is not using */
+	/* K_KERNEL_STACK_MEMBER(stack, CONFIG_ADC_TL323X_ACQUISITION_THREAD_STACK_SIZE); */
 };
 
 /* Driver configuration structure */
@@ -432,14 +433,14 @@ static int adc_tl323x_init(const struct device *dev)
     }
 
 	k_sem_init(&data->acq_sem, 0, 1);
-
-	k_thread_create(&data->thread, data->stack,
+	/* To save resources, the ADC thread will not be created because the user is not using */
+	/* k_thread_create(&data->thread, data->stack,
 		CONFIG_ADC_TL323X_ACQUISITION_THREAD_STACK_SIZE,
 		(k_thread_entry_t)adc_tl323x_acquisition_thread,
 		(void *)dev, NULL, NULL,
 		CONFIG_ADC_TL323X_ACQUISITION_THREAD_PRIO,
 		0, K_NO_WAIT);
-
+	*/
 	adc_context_unlock_unconditionally(&data->ctx);
 
 	return 0;
