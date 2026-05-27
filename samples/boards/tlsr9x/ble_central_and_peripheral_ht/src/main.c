029ce0c4c761c7982c18f03652b28343be02f15f
Zhihan Tao <zhihan.tao@telink-semi.com>
2026-05-27 15:18:44 +0800

    feat(tl3238x/zephyr): add support for c1p1 function.
    - modify the sample codes to be compatible with actual usage.
    
    Signed-off-by: Zhihan Tao <zhihan.tao@telink-semi.com>

--- samples/boards/tlsr9x/ble_central_and_peripheral_ht/src/main.c
+++ samples/boards/tlsr9x/ble_central_and_peripheral_ht/src/main.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <errno.h>
@@ -28,7 +29,8 @@ static uint8_t simulate_htm;
 static struct bt_gatt_indicate_params ind_params;
 static double temperature;
 
-static struct bt_conn *default_conn;
+static struct bt_conn *central_conn;
+static struct bt_conn *peripheral_conn;
 static struct bt_uuid_16 discover_uuid = BT_UUID_INIT_16(0);
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_subscribe_params subscribe_params;
@@ -156,8 +158,8 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 		if (conn_err) {
 			printk("Failed to connect to %s (%u)\n", addr, conn_err);
 
-			bt_conn_unref(default_conn);
-			default_conn = NULL;
+			bt_conn_unref(central_conn);
+			central_conn = NULL;
 
 			scan_start();
 			return;
@@ -165,7 +167,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 
 		printk("Connected: %s\n", addr);
 
-		if (conn == default_conn) {
+		if (conn == central_conn) {
 			memcpy(&discover_uuid, BT_UUID_HTS, sizeof(discover_uuid));
 			discover_params.uuid = &discover_uuid.uuid;
 			discover_params.func = discover_func;
@@ -173,7 +175,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 			discover_params.end_handle = BT_ATT_LAST_ATTRIBUTE_HANDLE;
 			discover_params.type = BT_GATT_DISCOVER_PRIMARY;
 
-			err = bt_gatt_discover(default_conn, &discover_params);
+			err = bt_gatt_discover(central_conn, &discover_params);
 			if (err) {
 				printk("Discover failed(err %d)\n", err);
 				return;
@@ -182,72 +184,54 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 	} else {
 		printk("connection type slave\r\n");
 
-		if (err) {
-			printk("Connection failed (err %u)\n", err);
+		if (conn_err) {
+			printk("Connection failed (err %u)\n", conn_err);
 		} else {
-			default_conn = bt_conn_ref(conn);
+			peripheral_conn = bt_conn_ref(conn);
 			printk("Connected\n");
 		}
 	}
 }
 
-static bool eir_found(struct bt_data *data, void *user_data)
-{
-	bt_addr_le_t *addr = user_data;
-	int i;
-
-	printk("[AD]: %u data_len %u\n", data->type, data->data_len);
-
-	switch (data->type) {
-	case BT_DATA_UUID16_SOME:
-	case BT_DATA_UUID16_ALL:
-		if (data->data_len % sizeof(uint16_t) != 0U) {
-			printk("AD malformed\n");
-			return true;
-		}
-
-		for (i = 0; i < data->data_len; i += sizeof(uint16_t)) {
-			const struct bt_uuid *uuid;
-			uint16_t u16;
-			int err;
-
-			memcpy(&u16, &data->data[i], sizeof(u16));
-			uuid = BT_UUID_DECLARE_16(sys_le16_to_cpu(u16));
-			if (bt_uuid_cmp(uuid, BT_UUID_HTS)) {
-				continue;
-			}
-
-			err = bt_le_scan_stop();
-			if (err) {
-				printk("Stop LE scan failed (err %d)\n", err);
-				continue;
-			}
-
-			err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN,
-						BT_LE_CONN_PARAM_DEFAULT, &default_conn);
-			if (err) {
-				printk("Create connection failed (err %d)\n", err);
-				scan_start();
-			}
-
-			return false;
-		}
-	}
-
-	return true;
-}
-
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad)
 {
-	char dev[BT_ADDR_LE_STR_LEN];
+	char addr_str[BT_ADDR_LE_STR_LEN];
+	int err;
 
-	bt_addr_le_to_str(addr, dev, sizeof(dev));
-	printk("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n", dev, type, ad->len, rssi);
+	if (central_conn) {
+		return;
+	}
 
 	/* We're only interested in connectable events */
-	if (type == BT_HCI_ADV_IND || type == BT_HCI_ADV_DIRECT_IND) {
-		bt_data_parse(ad, eir_found, (void *)addr);
+	if (type != BT_GAP_ADV_TYPE_ADV_IND &&
+	    type != BT_GAP_ADV_TYPE_ADV_DIRECT_IND) {
+		return;
+	}
+
+	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
+
+	/* connect only to devices in close proximity */
+	if (rssi < -50) {
+		return;
+	}
+
+	uint8_t addr_filter[BT_ADDR_SIZE] = {0xAC, 0x61, 0x6B, 0xF5, 0xBF, 0xC4};
+	if(memcmp(addr->a.val, addr_filter, BT_ADDR_SIZE) != 0) {
+		return;
+	}
+
+	printk("Device found: %s (RSSI %d)\n", addr_str, rssi);
+
+	if (bt_le_scan_stop()) {
+		return;
+	}
+
+	err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN,
+				BT_LE_CONN_PARAM_DEFAULT, &central_conn);
+	if (err) {
+		printk("Create conn to %s failed (%d)\n", addr_str, err);
+		scan_start();
 	}
 }
 
@@ -275,16 +259,17 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 
 	printk("Disconnected: %s, reason 0x%02x %s\n", addr, reason, bt_hci_err_to_str(reason));
 
-	if (default_conn != conn) {
-		return;
-	}
+	if (conn == central_conn) {
+		bt_conn_unref(central_conn);
+		central_conn = NULL;
 
-	bt_conn_unref(default_conn);
-	default_conn = NULL;
-
-	err = scan_start();
-	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
+		err = scan_start();
+		if (err) {
+			printk("Scanning failed to start (err %d)\n", err);
+		}
+	} else if (conn == peripheral_conn) {
+		bt_conn_unref(peripheral_conn);
+		peripheral_conn = NULL;
 	}
 }
 
