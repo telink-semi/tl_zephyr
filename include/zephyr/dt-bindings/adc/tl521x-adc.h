650538c403bc7be41f8be5ecaf9b31eba4b23861
Hao Wu <hao.wu@telink-semi.com>
2026-07-14 20:07:26 +0800

    drivers: telink: tl521x: adapt sd_adc and revert cca
    - adapt sd_adc: vbat and gpio mode
    - revert cca
    
    Signed-off-by: Hao Wu <hao.wu@telink-semi.com>
    Signed-off-by: Damien Ji <yinghao.ji@telink-semi.com>

---
+++ include/zephyr/dt-bindings/adc/tl521x-adc.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022-2026 Telink Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_TL521X_ADC_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_TL521X_ADC_H_
+
+#define DT_ADC_GPIO_PC0 0x01
+#define DT_ADC_GPIO_PC1 0x02
+#define DT_ADC_GPIO_PC2 0x03
+#define DT_ADC_GPIO_PC3 0x04
+#define DT_ADC_GPIO_PC4 0x05
+#define DT_ADC_GPIO_PC5 0x06
+#define DT_ADC_GPIO_PB4 0x07
+#define DT_ADC_GPIO_PB5 0x08
+#define DT_ADC_GPIO_PB6 0x09
+#define DT_ADC_GPIO_PB7 0x0a
+#define DT_ADC_VBAT     0x0f
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_TLX_ADC_H_ */
